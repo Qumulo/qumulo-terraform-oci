7d215fef31309d4624051e5bd0563b5ec8556847
Porter Smith <psmith@qumulo.com>
2025-08-11 10:41:28 -0700

    Clean up the qumulo-core.rpm object before attempting to form the cluster
    QFSD will in the future check if the persistent-storage buckets are empty first before allowing a cluster to be formed. Allowing the qumulo-core.rpm object to live in the bucket forever will cause clustering to fail on OCI.

--- core/modules/qprovisioner/scripts/provision.py
+++ core/modules/qprovisioner/scripts/provision.py
@@ -47,6 +47,8 @@ import requests
 # Template variables (replaced by Terraform templatefile)
 cluster_node_ip_addresses = "${cluster_node_ip_addresses}"
 clustering_node_ip_address = "${clustering_node_ip_address}"
+qumulo_core_rpm_bucket_name="${qumulo_core_rpm_bucket_name}"
+qumulo_core_rpm_object_name="${qumulo_core_rpm_object_name}"
 
 
 @dataclass
@@ -169,6 +171,17 @@ def wait_for_qfsd_installation(cluster_node_ip_addresses: str) -> None:
 
             logging.info(f"Waiting for QFSD to be up and running on node {ip}")
             time.sleep(10)
+
+
+def clean_up_qumulo_core_rpm(bucket_name: str, object_name: str) -> None:
+    if bucket_name != "" and object_name != "":
+        # If the object does not exist in the bucket, don't let that fail provisioning. If it fails
+        # to be deleted, qfsd will catch it later when the cluster is formed.
+        run_command(
+            f'/root/bin/oci os object delete --bucket-name "{bucket_name}" --object-name "{object_name}"'
+            "--auth instance_principal",
+            check=False
+        )
 
 
 def get_qfsd_version(ip: str) -> str:
@@ -524,6 +537,7 @@ def main() -> None:
     os.chdir("/root")
     install_oci_cli()
     wait_for_qfsd_installation(cluster_node_ip_addresses)
+    clean_up_qumulo_core_rpm(qumulo_core_rpm_bucket_name, qumulo_core_rpm_object_name)
     download_qq_client()
 
     qfsd_version = get_qfsd_version(clustering_node_ip_address)
